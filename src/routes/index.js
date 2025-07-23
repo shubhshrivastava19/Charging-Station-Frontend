885958d8df9c697994008bd32fcff8c4786005ab
shubhshrivastava19 <shubhshrivastav39@gmail.com>
2025-07-23 21:15:54 +0530

    Deploy Fix 3

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '@/components/Login.vue'
 import RegisterPage from '@/components/Register.vue'
+import DashboardPage from '@/components/Dashboard.vue'
 
 const routes = [
   { path: '/', component: LoginPage },
   { path: '/Register', component: RegisterPage },
-  // { path: '/dashboard', component: RegisterPage },
+  { path: '/dashboard', component: DashboardPage },
 ]
 
 const router = createRouter({
