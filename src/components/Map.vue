dc03dbbf8561db2230566d8b1116d28f1ea1afb0
shubhshrivastava19 <shubhshrivastav39@gmail.com>
2025-07-23 21:27:30 +0530

    fix map icons

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -8,6 +8,19 @@ import L from 'leaflet'
 import axios from 'axios'
 import 'leaflet/dist/leaflet.css'
 
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// Fix default icon path:
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow
+});
+
 let map;
 
 const getChargers = async () => {
