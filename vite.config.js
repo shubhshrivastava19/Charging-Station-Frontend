71b9b5b1dff6560480dd7f19ef9cbc13c60fc4c4
shubhshrivastava19 <shubhshrivastav39@gmail.com>
2025-07-23 21:00:15 +0530

    changed vite config file

--- vite.config.js
+++ vite.config.js
@@ -16,3 +16,10 @@ export default defineConfig({
     },
   },
 })
+
+// vue.config.js
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? 'Charging-Station-Frontend'
+    : '/'
+}
